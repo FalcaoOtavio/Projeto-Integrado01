5fe47c825551bc3462b3d0584e9eb64f25a73118
OtavioFalcao <otavioprogramador017@gmail.com>
2023-04-08 22:43:18 -0300

    v01

---
+++ Web Scraping/Web Scraping.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.binance.com/pt-BR/trade/BTC_USDT?theme=dark&type=spot'
+
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+
+site_binance = requests.get(url, headers)
+soup = BeautifulSoup(site_binance.content, 'html.parser')
+
+valor_btcusdt = soup.find('div', class_ = 'subPrice')
+
+print(valor_btcusdt)
